9374eb3cea1a95c2877ee1894f3ab60a9be6968f
virtual <iidai.informatica.unsxx@gmail.com>
2025-11-04 10:27:16 -0400

    Create dirigentes.js
    q

---
+++ historia/dirigentes.js
@@ -0,0 +1,121 @@
+var url1="https://cdn.glitch.global/25ac2c89-9dc7-4d65-8977-00a3df60e85c/";
+function texto_federico()
+{  
+   var texto="<div class='paneltexto'>" +
+          "<table>" +
+          "<tr>" +
+          "<td>" +
+          "<img src='"+url1+"federico2.jpg' width='200' height='250'><p>" +
+          "<img src='"+url1+"federico3.jpg' width='200' height='250'>" +
+          "</td>" +
+          "<td><h2>FEDERICO ESCOBAR ZAPATA<br>1923-1966</h2><p>" +
+          "Federico Escobar Zapata es un nombre vinculado en la historia del sindicalismo boliviano de los años 60. Nace en Oruro (Bolivia) el 26 de noviembre de 1923. Hijo de Francisco Eleuterio Escobar, obrero del ferrocarril Machacamarca – Uncía, y Nieves Zapata de Escobar.<p>" +
+          "Federico quedó huérfano de padre a temprana edad y pasó su niñez en Machacamarca. Estudió sus primeros años en el colegio Leónidas Lazarte y posteriormente en el colegio Simón Bolivar de Oruro.<p>" +
+          "A los 17 años comenzó a trabajar en la Empresa Minera Catavi perteneciente al campamento de Siglo XX, un centro minero de mucha importancia por la producción de estaño. Fue precisamente en Siglo XX donde Simón Patiño, el Rey del Estaño, encontró la veta de estaño más grande del mundo. Patiño fue, en aquel entonces, el industrial más importante de Sudamérica.<p>" +
+          "Desde muy joven se educó para liderar el movimiento obrero más combativo de Bolivia de aquella época. Participaba en asambleas sindicales, se nutría de lecturas revolucionarias, escuchaba hablar del Partido de la Izquierda Revolucionaria (PIR) y de muchas otras cosas referentes a la clase minera. En ese entonces no se había fundado el Partido Comunista Boliviano, pero la oligarquía comentaba sobre la existencia de “agitadores profesionales pertenecientes a un extremismo comunista”<p>" +
+          "Federico Escobar ocupó el cargo de Control Obrero de Siglo XX elegido democráticamente por las bases. El aporte más grande de Federico Escobar; es haber puesto, en Siglo XX, los cimientos de una poderosa organización marxista leninista en abril de 1965, cuando participó en un Congreso como el líder de los mineros. Más de 500 delegados asistieron a esta reunión del proletariado. Se estaba creando, entonces, un verdadero partido político auténtico de la clase obrera. Ningún otro partido político había nacido en las entrañas mismas de la clase obrera boliviana<p>" +
+          "Lamentablemente la obra de Federico Escobar se trunca a causa de su prematura muerte el 8 de noviembre de 1966. Una muerte que ha quedado en un gran enigma. Federico Escobar fue ingresado a una clínica que pertenecía a la Corporación Minera de Bolivia (COMIBOL) por fractura de un brazo y, misteriosamente, salió sin vida (Claure, 2009)" +
+          "</td>" +
+          "</tr>" +
+          "</table>" +
+          "Haga click para continuar" +
+          "</div>";    
+  return texto;
+}
+function texto_ireneo()
+{  
+   var texto="<div class='paneltexto'>" +
+          "<table>" +
+          "<tr>" +
+          "<td>" +
+          "<img src='"+url1+"ireneo.jpg' width='200' height='250'>" +
+          "</td>" +
+          "<td><h2>IRENEO PIMENTEL ROJAS<br>1297-1978</h2><p>" +
+          "Ireneo Pimentel Rojas, nace el en Siglo XX el 1 de abril de 1927.<p>" +
+          "Entre 1954 y 1965 fue secretario general del Control Obrero, En su función trató de lograr mejores condiciones de vida y trabajo para los trabajadores mineros<p>A su mérito se debe anotar la construcción del edificio sindical y la Plaza del Minero, donde posteriormente erigió el monumento a Federico Escobar Zapata –su compañero de lucha-, igualmente construyó el estadio María Barzola que hoy lleva su nombre<p>" +
+          "Fue víctima de masacre blanca de la Empresa Patiño Mines & Enterprises Consolidated, Inc.<p>Perseguido, apresado y exiliado por los dictadores Barrientos y Banzer<p>En 1974 Banzer desconoce a los sindicatos e impone coordinadores laborales en su reemplazo, situación rechazada por la Federación de Mineros. El dictador ordena la prisión de los dirigentes mineros Víctor López, Alberto Jara, Irineo Pimentel y otros, en la carceleta de San Pedro y en Chonchocoro, por desobedecer  a Ley de Servicio Civil Obligatorio, falleció en el año 1978Oporto (2006) <p>" +
+          "</td>" +
+          "</tr>" +
+          "</table>" +
+          "Haga click para continuar" +
+          "</div>";
+  return texto;
+}
+function texto_issac()
+{  
+   var texto="<div class='paneltexto'>" +
+          "<table>" +
+          "<tr>" +
+          "<td>" +
+          "<img src='"+url1+"issac2.jpg' width='200' height='250'>" +
+          "</td>" +
+          "<td><h2>ISSAC CAMACHO<br>1933-1967</h2><p>" +
+          "Nació en la población de Llallagua en 1933 y estudió en el Instituto Americano de la ciudad de La Paz. Ingresó a trabajar a la mina de Siglo XX, en la sección Block-kaving, en febrero de 1957. Desde entonces se perfiló como un auténtico dirigente político y sindical.<p>" +
+          "Isaac Camacho, un mes después de denunciar a los culpables del asesinato de su compañero y camarada inseparable César Lora, acaecido el 29 de julio de 1965, en las cercanías de San Pedro de Buena Vista, fue apresado, conducido al campo de concentración de Alto Madidi y encerrado en las celdas del Panóptico Nacional, de donde fue liberado por una fuerte presión popular.<p>" +
+          "A su retorno a Llallagua, Isaac Camacho prosiguió su lucha contra la dictadura militar de Rene Barrientos Ortuño, en su calidad de dirigente de los “sindicatos clandestinos”. Así se mantuvo hasta la noche del 23 de junio de 1967, en que se dio inicio a la tradicional fogata de San Juan, encendiendo leña y trastos viejos en las calles, mientras alrededor del crepitante fuego se reunían las familias mineras, haciendo tronar juegos artificiales y brindando en la noche más frígida del año<p>" +
+          "Un mes después de la “masacre de San Juan”, fue apresado y desaparecido por las fuerzas de represión del Estado boliviano. Cuando los mineros y su esposa reclamaron por su ausencia, el Ministro del Interior, Antonio Arguedas, dijo que el 9 de agosto fue embarcado rumbo a Argentina. Nada más falso. Se removió cielo y tierra, y no se lo volvió a encontrar ni vivo ni muerto. Desapareció para siempre<p>" +
+          "</td>" +
+          "</tr>" +
+          "</table>" +
+          "Haga click para continuar" +
+          "</div>";
+  return texto;
+}
+function texto_filemon()
+{  
+   var texto="<div class='paneltexto'>" +
+          "<table>" +
+          "<tr>" +
+          "<td>" +
+          "<img src='"+url1+"filemon.jpg' width='200' height='250'>" +
+          "</td>" +
+          "<td><h2>FILEMON ESCOBAR JIMENEZ<br>1934-2017</h2><p>" +
+          "Filemón Escobar nació el 26 de octubre de 1934 en la localidad de Uncía en el Departamento de Potosí.1 Quedó huérfano a los 12 años y fue internado en el orfanato Méndez Arcos en la ciudad de La Paz. Allí se inició en la política conociendo al entonces Presidente Gualberto Villarroel, que contó con el respaldo de campesinos y mineros.<p>" +
+          "A la edad de 21 años fue elegido como Secretario de Cultura del Sindicato de Mineros de Siglo XX. También fue dirigente de la Federación Sindical de Trabajadores Mineros de Bolivia, FSTMB, después que Juan Lechín Oquendo dejó el mando y dirigente de la Central Obrera Boliviana (COB), entidad por la que abogó como instancia suprapartidaria capaz de activar la revolución<p>" +
+          "El 21 de agosto de 1986, cuando fue secretario general de los trabajadores de Catavi2 junto a Simón Reyes encabezó la Marcha por la Vida en contra de la relocalización de mineros durante el Gobierno de Víctor Paz Estenssoro. Militó en el Partido Obrero Revolucionario (POR) y posteriormente, en Vanguardia Obrera. Fue candidato a la vicepresidencia con Genaro Flores en las elecciones de 1985 por el Movimiento Revolucionario Túpac Katari de Liberación, MRTKL, e ideólogo y fundador del Movimiento al Socialismo (Bolivia). En la política se lo conoció coloquialmente como Filipo<p>" +
+          "Murió el 6 de junio de 2017, a la edad de 82 años en la localidad de Tiquipaya, Cochabamba, afectado por cáncer de pulmón fue sepultado en el Cementerio General de Cochabamba.<p>" +
+          "</td>" +
+          "</tr>" +
+          "</table>" +
+          "Haga click para continuar" +
+          "</div>";
+  return texto;
+}
+function texto_domitila()
+{  
+   var texto="<div class='paneltexto'>" +
+          "<table>" +
+          "<tr>" +
+          "<td>" +
+          "<img src='"+url1+"domitila.jpg' width='200' height='250'>" +
+          "</td>" +
+          "<td><h2>DOMITILA CHUNGARA<br>1937-2012</h2><p>" +
+          "Domitila Barrios nació el 7 de mayo de 1937 en la comunidad Catavi, cerca de la mina Siglo XX.1 Cuando tenía diez años, los rigores de la vida en el pueblo minero acabaron con la vida de su madre por lo que tuvo que hacerse cargo del cuidado de sus cinco hermanas menores, pues su padre pasaba todo el día trabajando como sastre de la policía minera. Empezó su trabajo en la mina como palliri.<p>" +
+          "En 1952, como esposa de un trabajador minero, fue parte del Comité de Amas de Casa del Distrito Minero Siglo XX. Ya entonces su liderazgo era evidente, lo que la llevó a ser designada Secretaria General de ese organismo<p>" +
+          "Salió al exilio en a década de los ochenta, pero pronto volvió a Bolivia y se instaló en Cochabamba, donde impulsaba un centro de formación política especialmente destinada a las jóvenes de los barrios más empobrecidos de esta ciudad<p>" +
+          "en junio de 1967, el presidente René Barrientos Ortuño envió un contingente militar a las comunidades de Catavi y Llallagua, para reprimir las reivindicaciones de los mineros parapetados contra la explotación y los abusos de los grandes empresarios. Tras la matanza, Domitila Barrios Cuenca fue apresada y torturada por los militares. A consecuencia de estos abusos perdió el bebé que llevaba dentro de su vientre. Estos hechos se conocen como la Masacre de San Juan<p>" +
+          "A principios de 1978, Barrios fue convocada para formar parte del segundo piquete de huelguistas que instaló el sacerdote Luis Espinal en predios del periódico Presencia junto con el sacerdote Xavier Albó para apoyar la huelga de hambre que instalaron previamente cuatro mujeres mineras contra la dictadura; y en poco tiempo, más de 1.500 personas se sumaron a la huelga. Con el pasar de las horas, los huelguistas se multiplicaron por miles, y al régimen militar no le quedó otra opción salvo la de claudicar en favor de la democracia.<p>" +
+          "Apoyó a doblegar a la dictadura militar del Gral. Hugo Banzer Suárez obligándole a iniciar una verdadera apertura democrática y no un simulacro, como era lo originalmente planeado.<p>" +
+          "Falleció de cáncer de pulmón en la ciudad de Cochabamba el 13 de marzo de 2012 a los 74 años de edad, posteriormente el Gobierno Nacional de Bolivia declaró 3 días de duelo nacional.<p>";
+        "</td>" + "</tr>" + "</table>" + "Haga click para continuar" + "</div>";
+  return texto;
+}
+function texto_cirilo()
+{  
+   var texto="<div class='paneltexto'>" +
+          "<table>" +
+          "<tr>" +
+          "<td>" +
+          "<img src='"+url1+"cirilo.jpg' width='200' height='250'>" +
+          "</td>" +
+          "<td><h2>CIRILO JIMENEZ<br>1930-2018</h2><p>" +
+          "irilo Jiménez Alvarez Nació en Tacaraní, Potosí en 1930, en 1952 ingresó a trabajar como empleado de Bienestar de la Patiño Mines, donde se mantuvo por 8 años. Posteriormente pasó a la Planta Sink & Flota, y trabajó en interior mina. Fue dirigente sindical en las carteras de Organización, Vinculación Campesina y Conflictos en el Sindicato Mixto de Trabajadores Mineros de Siglo XX.<p>" +
+          "En la Federación Sindical de Trabajadores Mineros de Bolivia (FSTMB) fue secretario de Deportes (1982-1984) y Conflictos (1985-1993). Fundador y primer secretario de la Central Obrera Regional del Norte de Potosí. Como resultado de su actividad sindical fue retirado de la empresa, confinado y perseguido por las dictaduras militares<p>" +
+          "Cirilo Jiménez Alvarez destaca principalmente por se el impulsor de la creación de la Universidad Nacional “Siglo XX”, de la que fue su Vicerrector y posteriormente llego a ser Rector.<p>" +
+          "</td>" +
+          "</tr>" +
+          "</table>" +
+          "Haga click para continuar" +
+          "</div>";
+  return texto;
+}
